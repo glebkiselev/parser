57da489013dd6a7b58e6921db4de31138ebaa32a
gleb <glebkiselev777@mail.ru>
2017-03-21 20:13:04 +0300

    csv writer + some refactoring

--- search.py
+++ search.py
@@ -7,76 +7,20 @@ import itertools
 from bs4 import BeautifulSoup
 import csv
 
-from openpyxl import Workbook
-from werkzeug.utils import secure_filename
-import os
-import os.path
-from flask import send_from_directory
-import flask_excel as excel
 from flask import Flask
 from flask import request, render_template, redirect, url_for, jsonify
 from openpyxl import load_workbook
 import pandas
+from multiprocessing import Pool, Process
+from multiprocessing.dummy import Pool as ThreadPool
+import subprocess
+
 
 UPLOAD_FOLDER = 'data'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'csv', 'xls', 'xlsx', 'docx'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-#
-# def allowed_file(filename):
-#     return '.' in filename and \
-#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-#
-# @app.route('/up', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit a empty part without filename
-#         if file.filename == '':
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('uploaded_file', filename=filename))
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <p><input type=file name=file>
-#          <input type=submit value=Upload>
-#          <a href = "/upload"> LOOK File </a>
-#     </form>
-#     '''
-
-
-# @app.route('/data/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename)
-#
-#
-# @app.route('/upload/', methods=['GET', 'POST'])
-# def uploadu_file():
-#     if request.method == 'POST':
-#         return render_template('upfile.html',
-#                                result_tab={"result": request.get_array(field_name='file', encoding="utf-8")})
-#
-#     return render_template('upfile.html')
-#
-#
-# @app.route('/export', methods=['GET'])
-# def export_records():
-#     return excel.make_response_from_array([[1, 2], [3, 4]], 'xlsx',
-#                                           file_name="file", encoding="ISO-8859-1")
-
 
 @app.route('/app.js')
 def sencha_app():
@@ -170,14 +114,17 @@ def get_text(html):
         return " "
 
 def read_url(url):
-    with urlopen(url) as data:
-        enc = data.info().get_content_charset('utf-8')
-        try:
-            html = data.read().decode(enc)
-        except UnicodeDecodeError:
-            print("bad unicode decoding")
-            return None
-
+    try:
+        with urlopen(url) as data:
+            enc = data.info().get_content_charset('utf-8')
+            try:
+                html = data.read().decode(enc)
+            except UnicodeDecodeError:
+                print("bad unicode decoding")
+                return None
+    except UnicodeEncodeError:
+        print("bad unicode decoding")
+        return None
     return html
 
 
@@ -235,98 +182,88 @@ def main_alg(url, link, words, posts, visited_links, depth):
 
 @app.route('/_findwords')
 def add_numbers():
-    #urls = request.args.get('url')
-    # read from test_check keywords and links
+    #get links and keywords
     a= execsear()
     urls=[]
     urls.extend(a[0])
-    # word1 = request.args.get('word1')
-    # word2 = request.args.get('word2')
-    # word3 = request.args.get('word3')
-    # value = ''
     words = []
     words.extend(a[1])
 
-    # words = [str(word1), str(word2), str(word3)]
-    # if len(word1.strip()) > 0:
-    #    words.append(word1)
-    # if len(word2.strip()) > 0:
-    #    words.append(word2)
-    # if len(word3.strip()) > 0:
-    #    words.append(word3)
-  # список найденных постов
     depth = 2  # размерность поиска вглубину (кол-во страниц сайта, которые мы просмотрим)
-
-
     # read from csv marks and models
-
     cars_file = "static/cars_csv.csv"
-
     marks_and_models = read_csv(cars_file)
-
-
     filename = 'reader.xlsx'
 
     if len(urls) > 1:
-        for url in urls:
-            posts = []
-            visited_links = [url]  # was here
-            if not url.startswith("http://") and not url.startswith("https://"):
-                url = 'http://' + url
-            if url[len(url)-1] == " ":
-                url = del_probel(url)
-            posts = set(main_alg(url, url, words, posts, visited_links, depth))
-            print("Im in alg!")
-            good = []
-            costs = ["cost", "цен", "скид", "руб", "процент", "клиент", "%"]
-            compons = list(itertools.product(marks_and_models, costs))
-            posts = list(posts)
-            for post in posts:
-                strings = post[1].split(".")
-                for string in strings:
-                    for compon in compons:
-                        if compon[0][0] in string and compon[0][1] in post[1] and compon[1] in post[1]:
-                            good.append((post[0], compon[0][0], compon[0][1], post[1]))
+            # разбиваю по 10 итого 50 url
+            urls_for_process = list(group(urls, 5))
+            iterater = 0
+            for url in urls_for_process:
+                filepath = str(iterater) + ".csv"
+                subprocess.call(['touch', filepath])
+                post_searcher(url, words, depth, marks_and_models, filepath)
+                iterater+=1
 
 
-            # for post in copy:
-            #     strings = post[1].split(".")
-            #     copy2 = strings.copy()
-            #     for string in copy2:
-            #         if not any(words[0] in string.lower() and words[1] in string.lower() for words in compon):
-            #             strings.remove(string)
-            #         else:
-            #             good.append((post[0], string))
-            # if good:
-            #     df = pandas.DataFrame([good])
-            # else:
 
-            labels = ['link', 'mark', 'model', 'post']
-            df = pandas.DataFrame.from_records(good, columns=labels)
-            book = load_workbook(filename)
-            writer = pandas.ExcelWriter(filename, engine='openpyxl')
-            writer.book = book
-            writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
-            df.to_excel(writer, "Main", columns=labels)
 
-            writer.save()
-            # with pandas.ExcelWriter('reader.xlsx', engine='openpyxl') as writer:
-            #     writer.book = load_workbook('reader.xlsx')
-            #     df.to_excel(writer, "Main")
 
-            # with open('file.xlsx', 'w') as out:
-            #     csv_out = csv.writer(out)
-            #     csv_out.writerow(['link', 'text'])
-            #     for row in posts:
-            #         csv_out.writerow(row)
-            # str_to_serv = ''
-            # for post in posts:
-            #     str_to_serv = str(post) + str_to_serv
-            # return jsonify(result=str_to_serv)
+def group(iterable, count):
+    return zip(*[iter(iterable)] * count)
+
+
+
+
+def post_searcher(urls, words, depth, marks_and_models, filename):
+    for url in urls:
+        posts = []
+        visited_links = [url]  # was here
+        if not url.startswith("http://") and not url.startswith("https://"):
+            url = 'http://' + url
+        if url[len(url) - 1] == " ":
+            url = del_probel(url)
+        posts = set(main_alg(url, url, words, posts, visited_links, depth))
+        if len(posts) < 10:
+            continue
+        print("url:{0}  for post len: {1}".format(url, len(posts)))
+        good = []
+        costs = ["cost", "цен", "скид", "руб", "процент", "клиент", "%"]
+        compons = list(itertools.product(marks_and_models, costs))
+        posts = list(posts)
+        for post in posts:
+            strings = post[1].split(".")
+            for string in strings:
+                for compon in compons:
+                    if compon[0][0] in string and compon[0][1] in post[1] and compon[1] in post[1]:
+                        good.append((post[0], compon[0][0], compon[0][1], post[1]))
+
+        # to write for excisting xlsx
+        #write_xlsx(good, filename)
+        # to wite for excisting csv
+        write_csv(good, filename)
+
+def write_csv(good, filename):
+    with open(filename, 'a') as resultFile:
+        wr = csv.writer(resultFile, dialect='excel')
+        wr.writerow(good)
+
+
+def write_xlsx(good, filename):
+    labels = ['link', 'mark', 'model', 'post']
+    df = pandas.DataFrame.from_records(good, columns=labels)
+    book = load_workbook(filename)
+    writer = pandas.ExcelWriter(filename, engine='openpyxl')
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    df.to_excel(writer, "Main", columns=labels)
+
+    writer.save()
+
 
 def read_csv(cars_file):
-
     with open(cars_file, encoding='mac_roman') as f:
         datas = [line for line in csv.reader(f)]
     list_data = []
@@ -364,23 +301,6 @@ def execsear():
 
     return [links, words]
 
-
-def run(url, words):
-    posts = []  # список найденных постов
-    depth = 3  # размерность поиска вглубину (кол-во страниц сайта, которые мы просмотрим)
-    visited_links = [url]  # was here
-    print(url)
-    print(words)
-    posts = set(main_alg(url, url, words, posts, visited_links, depth))
-    # with open('file.csv', 'w') as csvfile:
-    #     spamwriter = csv.writer(csvfile,  delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, dialect='excel')
-    #     spamwriter.writerow(posts)
-    with open('file.csv', 'w') as out:
-        csv_out = csv.writer(out)
-        csv_out.writerow(['link', 'text'])
-        for row in posts:
-            csv_out.writerow(row)
-            # print(posts)
 
 
 if __name__ == '__main__':
